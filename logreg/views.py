2c24669e0214a4f6dd9c0adf0cb82d0afab8af39
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-11 15:46:14 +0200

    Templates

--- logreg/views.py
+++ logreg/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def signup(request):
-    return HttpResponse('Hello world! This is registration page')
-
-
 def login(request):
-    return HttpResponse('Hello world! This is login page')
+    return render(request, 'logreg/login.html')
+
+
+def signup(request):
+    return render(request, 'logreg/reg.html')
+    # return HttpResponse('Hello world! This is registration page')
