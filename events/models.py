156498e549dbe5bf583af4621e8b8b3a1c106fa2
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-14 21:24:09 +0200

    changes to models

--- events/models.py
+++ events/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Event(models.Model):
     event_name = models.CharField(max_length=30, blank=True, null=True)
     event_description = models.TextField(blank=True, null=True)
-    event_photo = models.ImageField(blank=True, null=True)
+    event_photo = models.ImageField(blank=True, null=True, upload_to='media/events')
     event_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
--- logreg/models.py
+++ logreg/models.py
@@ -7,11 +7,11 @@ class User(models.Model):
     last_name = models.CharField(max_length=30, blank=True, null=True)
     password = models.CharField(max_length=30, blank=True, null=True)
     sex = models.IntegerField(blank=True, null=True)
-    family_status = models.IntegerField(blank=True, null=True)
+    family_status = models.IntegerField(default=0, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField(blank=False, null=False, primary_key=True)
 
-    organizer = models.BooleanField(blank=True, null=True)
+    organizer = models.BooleanField(default=False, blank=False, null=False)
 
     class Meta:
         verbose_name = 'User'
