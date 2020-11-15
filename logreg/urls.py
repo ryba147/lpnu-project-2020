6c648c0355a22eeac63f3020a0e5dc548c38e50f
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-15 15:35:02 +0200

    new

--- logreg/urls.py
+++ logreg/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='log'),
-    path('signup/', views.signup, name='reg'),
+    path('user/', views.user_api),
+    path('user/<str:email>/', views.user_api),
 ]
--- logreg/views.py
+++ logreg/views.py
@@ -1,12 +1,60 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
-def login(request):
-    return render(request, 'logreg/login.html')
+
+# # Create your views here.
+# def login(request):
+#     return render(request, 'logreg/login.html')
+#
+#
+# def signup(request):
+#     return render(request, 'logreg/reg.html')
+#     # return HttpResponse('Hello world! This is registration page')
 
 
-def signup(request):
-    return render(request, 'logreg/reg.html')
-    # return HttpResponse('Hello world! This is registration page')
+@csrf_exempt
+def user_api(request, email="123@example.com"):
+    if request.method == "GET":
+        users = User.objects.all()
+        users_serializer = UserSerializer(users, many=True)
+        return JsonResponse(users_serializer.data, safe=False)
+
+    elif request.method == "POST":
+        user_data = JSONParser().parse(request)
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return JsonResponse("New user was created successfully", safe=False)
+        else:
+            return JsonResponse("Failed to create user", safe=False)
+
+    elif request.method == "PUT":
+        user_data = JSONParser().parse(request)
+        user = User.objects.get(email=user_data['email'])  # щоб знати конкретного юзера, інфу якого змінюватимемо
+        user_serializer = UserSerializer(user, data=user_data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return JsonResponse("User information was updated successfully", safe=False)
+        else:
+            return JsonResponse("Failed to update user information", safe=False)
+
+    elif request.method == "DELETE":
+        user = User.objects.get(email=email)
+        user.delete()
+        return JsonResponse("User account was deleted successfully", safe=False)
+
+# {
+#     "first_name": "Taras",
+#     "last_name": "Bmx",
+#     "password": "1234",
+#     "email": "1@example.com",
+#     "sex": 1,
+#     "family_status": 0,
+#     "organizer": 1
+# }
