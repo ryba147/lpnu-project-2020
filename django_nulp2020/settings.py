b1bf158e3421ddfa0738d389cf03f338c470c9ff
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-11-16 20:42:40 +0200

    added swagger

--- django_nulp2020/settings.py
+++ django_nulp2020/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'logreg',
     'events',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
--- events/views.py
+++ events/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
 
 
 # Create your views here.
 def events(request):
     events_list = Event.objects.all()
     return render(request, 'events/view_events.html', {'events_list': events_list})
+
+
+
+schema_view = get_swagger_view(title='Pastebin API')
+
+urlpatterns = [
+    url(r'^$', schema_view)
+]
