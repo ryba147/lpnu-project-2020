57a655b5758f587b42092e649d94ddf68729297b
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-17 01:00:59 +0200

    CRUD update

--- logreg/models.py
+++ logreg/models.py
@@ -5,13 +5,29 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=30, blank=False, null=True)
     last_name = models.CharField(max_length=30, blank=False, null=True)
-    password = models.CharField(max_length=30, blank=False, null=True)
-    sex = models.IntegerField(blank=True, null=True)
-    family_status = models.IntegerField(default=0, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
-    email = models.EmailField(blank=False, null=False, primary_key=True)
+    city = models.CharField(max_length=30, blank=True, null=True)
+    password = models.CharField(max_length=30, blank=False, null=True)
 
+    SEX_CHOICES = (
+        (0, 'Male'),
+        (1, 'Female'),
+    )
+    sex = models.IntegerField(blank=True, null=True, choices=SEX_CHOICES)
+
+    FAMILY_STATUS = (
+        (0, 'Single'),
+        (1, 'In relationship'),
+        (2, 'Married')
+    )
+    family_status = models.IntegerField(blank=True, null=True, choices=FAMILY_STATUS)
+
+    pets = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=False, null=False, primary_key=True)
     organizer = models.BooleanField(default=False, blank=False, null=False)
+
+    def __str__(self):
+        return self.first_name
 
     class Meta:
         verbose_name = 'User'
--- logreg/views.py
+++ logreg/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
@@ -19,35 +20,51 @@ from .serializers import UserSerializer
 
 
 @csrf_exempt
-def user_api(request, email="123@example.com"):
-    if request.method == "GET":
+def user_api(request, email=""):
+    # if request.method == "GET":
+    #     users = User.objects.all()
+    #     users_serializer = UserSerializer(users, many=True)
+    #     return JsonResponse(users_serializer.data, safe=False)
+
+    # if email is not specified in address line by http://127.0.0.1:8080/user/some-email
+    # then django returns data of ALL objects in json
+    if email == "" and request.method == "GET":
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return JsonResponse(users_serializer.data, safe=False)
+    else:
+        # search the specified email and return data in json
+        if request.method == "GET":
+            try:
+                user = User.objects.get(email=email)
+                user_serializer = UserSerializer(user, many=False)
+                return JsonResponse(user_serializer.data, safe=False)
+            except ObjectDoesNotExist:
+                return JsonResponse("User does not exists", safe=False)
 
-    elif request.method == "POST":
-        user_data = JSONParser().parse(request)
-        user_serializer = UserSerializer(data=user_data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return JsonResponse("New user was created successfully", safe=False)
-        else:
-            return JsonResponse("Failed to create user", safe=False)
+        elif request.method == "POST":
+            user_data = JSONParser().parse(request)
+            user_serializer = UserSerializer(data=user_data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return JsonResponse("New user was created successfully", safe=False)
+            else:
+                return JsonResponse("Failed to create user", safe=False)
 
-    elif request.method == "PUT":
-        user_data = JSONParser().parse(request)
-        user = User.objects.get(email=user_data['email'])  # щоб знати конкретного юзера, інфу якого змінюватимемо
-        user_serializer = UserSerializer(user, data=user_data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return JsonResponse("User information was updated successfully", safe=False)
-        else:
-            return JsonResponse("Failed to update user information", safe=False)
+        elif request.method == "PUT":
+            user_data = JSONParser().parse(request)
+            user = User.objects.get(email=user_data['email'])  # щоб знати конкретного юзера, інфу якого змінюватимемо
+            user_serializer = UserSerializer(user, data=user_data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return JsonResponse("User information was updated successfully", safe=False)
+            else:
+                return JsonResponse("Failed to update user information", safe=False)
 
-    elif request.method == "DELETE":
-        user = User.objects.get(email=email)
-        user.delete()
-        return JsonResponse("User account was deleted successfully", safe=False)
+        elif request.method == "DELETE":
+            user = User.objects.get(email=email)
+            user.delete()
+            return JsonResponse("User account was deleted successfully", safe=False)
 
 # {
 #     "first_name": "Taras",
