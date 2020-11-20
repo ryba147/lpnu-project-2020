f2801badabc0f3686b5202bd53c716077adebf56
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-11-20 12:33:30 +0200

    Added user class

---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User{
+    first_name: string;
+    last_name: string;
+    birth_date: string;
+    email:string;
+    password:string;
+    city:string;
+    sex:number;
+    family_status:number;
+    pets:[];
+    organizer:boolean;
+    rating:number;
+
+}
