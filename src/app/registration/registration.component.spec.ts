0799b002665ae403c83003039c18815be375f9b4
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-21 13:11:43 +0200

    Sign up form validation

--- src/app/registration/registration.component.spec.ts
+++ src/app/registration/registration.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegistrationComponent } from './registration.component';
 
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+
+function passwordMatcher(c: AbstractControl) {
+  return c.get('password').value === c.get('confirm_password').value
+    ? null
+    : { nomatch: true };
+}
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form: FormGroup;
+  constructor(public fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      email: [
+        '',
+        [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')],
+      ],
+      pword: this.fb.group({
+          password: ['', Validators.required], //
+          confirm_password: ['', Validators.required],
+        }, { validator: passwordMatcher }),
+    });
   }
+  get f() { return this.form.controls; }
 
+  ngOnInit(): void {}
 }
