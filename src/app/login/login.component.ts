39750e63d460832cabcb81b06caef857aeb6b8de
bidaky <55603024+bidaky@users.noreply.github.com>
2020-11-22 03:28:07 +0200

    front

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
   constructor() { }
-
+  get loginEmail(){
+ return this.userEmails.get('loginEmail')
+  }
+  userEmails = new FormGroup({
+    loginEmail: new FormControl('',[
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    secondaryEmail: new FormControl('',[
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+    });
   ngOnInit(): void {
+
   }
 
 }
