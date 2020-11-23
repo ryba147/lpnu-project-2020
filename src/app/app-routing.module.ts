80803d76eb07a0ca5e1b322195036698ed9c791b
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-11-24 01:17:30 +0200

    Added privacy policy and contact us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
 import { LoginComponent } from './login/login.component';
 import { TitlePageComponent } from './title-page/title-page.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { HomePageComponent } from './home-page/home-page.component'
+import { from } from 'rxjs';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 const appRoutes: Routes = [
   { path: '', component: TitlePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: RegistrationComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'contact-us',component:ContactUsComponent},
+  { path: 'privacy-policy',component:PrivacyPolicyComponent},
 ];
 
 @NgModule({
