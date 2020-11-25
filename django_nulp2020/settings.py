7d624e55654785f27700f1dde134ea8c1ea1e46a
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-25 22:14:52 +0200

    POSTGRESQL setup

--- django_nulp2020/settings.py
+++ django_nulp2020/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'django_nulp2020.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eventuredb',
+        'USER': 'postgres',
+        'PASSWORD': 'pgnulp',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
