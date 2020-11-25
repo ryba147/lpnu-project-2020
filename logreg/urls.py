cbe8525b83d95872df4aada601680d087d73517b
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-25 23:41:59 +0200

    views.py UserView fix001

--- logreg/urls.py
+++ logreg/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.user_api),
-    path('user/<str:email>/', views.user_api),
+    path('user/', views.UserView),
+    path('user/<str:email>/', views.UserView),
 ]
