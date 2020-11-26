981951445c057c5039b696c158708a5a8e26e4c6
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-11-26 03:46:35 +0200

    Updated login service

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
-  constructor() { }
+
+  email:string;
+  pwd:string;
+  response:any;
+  constructor(private http:HttpClient,private router:Router) { }
+  
   get loginEmail(){
- return this.userEmails.get('loginEmail')
+    return this.userEmails.get('loginEmail')  
   }
+
   userEmails = new FormGroup({
     loginEmail: new FormControl('',[
       Validators.required,
@@ -18,7 +27,17 @@ export class LoginComponent implements OnInit {
     secondaryEmail: new FormControl('',[
       Validators.required,
       Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
-    });
+  });
+
+  enter(){
+    this.http.post('http://127.0.0.1:8080/user/'+this.email+'/',{'email':this.email}).subscribe(data =>{
+      if(data!='User does not exist!'){
+        this.router.navigate(['/home']);
+      }
+    })
+    
+  }
+
   ngOnInit(): void {
 
   }
