e311854034380b7b97ff0741762a2010f3386755
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-12-05 22:45:31 +0200

    updated user fields, header-dropdown

--- src/app/comment.ts
+++ src/app/comment.ts
@@ -1,9 +1,8 @@
-import { from } from 'rxjs';
-import {User} from './user';
-import {Event} from './event';
-export class Comment{
-    event:Event;
-    author:User;
-    comment_text:string;
-    pub_date:Date;
+import { User } from "./user";
+import { Event } from "./event";
+export class Comment {
+  event: Event;
+  author: User;
+  comment_text: string;
+  pub_date: Date;
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,15 +1,13 @@
-import { Data } from '@angular/router';
-
-export class User{
-    first_name: string;
-    last_name: string;
-    birth_date: Date;
-    email:string;
-    password:string;
-    city:string;
-    sex:number;
-    family_status:number;
-    pets:string;
-    organizer:boolean;
-    rating:number;
+export class User {
+  first_name: string;
+  last_name: string;
+  birth_date: Date;
+  email: string;
+  password: string;
+  city: string;
+  sex: string;
+  family_status: string;
+  pets: string;
+  organizer: boolean;
+  rating: number;
 }
