4b93151c965494788ea3676114cbd362cbbc74c9
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-12-06 18:47:47 +0200

    Readme, swagger

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # lpnu-project-2020
+*Running and stopping using python development server*<br>
 ```python manage.py runserver 8080```<br>
-http://localhost:8080/login/ <br>
-http://localhost:8080/signup/
+http://localhost:8080/user/ <br>
+
+use ```POST``` on -> http://127.0.0.1:8080/user/ (to create use JSON) <br>
+use ```POST``` on -> http://127.0.0.1:8080/user/ANY-EMAIL/  (to check if user with specified email is in db) <br>
+use ```DELETE``` on -> http://127.0.0.1:8080/user/ANY-EMAIL/ (to delete user with specified email from db) <br>
+use ```GET``` on -> http://127.0.0.1:8080/user/ (to get userlist from db) <br>
+use ```PUT``` on -> http://127.0.0.1:8080/user/ (use JSON to update info. do not forget to CREATE before EDITing) <br><br>
+JSON body:
+```
+{
+    "first_name": "Taras",
+    "last_name": "Vilinskyi",
+    "city": "Lviv",
+    "password": "pass1",
+    "sex": "male",
+    "family_status": "single",
+    "pets": "dog, cat, parrot",
+    "email": "2@ex.com",
+    "organizer": false,
+    "rating": 86.5
+}
+```
--- logreg/views.py
+++ logreg/views.py
@@ -17,13 +17,14 @@ class UserView(View):
     def dispatch(self, request, *args, **kwargs):
         return super(UserView, self).dispatch(request, *args, **kwargs)
 
-    def make_response(self,body):
-    	response = HttpResponse(body)
-    	response['Access-Control-Allow-Origin'] = '*'	
-    	response['Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers"
-    	response['Content-Type'] = 'application/json'
-    	response['allow'] = ",".join(self.allowed_methods)
-    	return response
+    def make_response(self, body):
+        response = HttpResponse(body)
+        response['Access-Control-Allow-Origin'] = '*'
+        response[
+            'Access-Control-Allow-Headers'] = "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers"
+        response['Content-Type'] = 'application/json'
+        response['allow'] = ",".join(self.allowed_methods)
+        return response
 
     def options(self, request, email=""):
         return self.make_response('')
@@ -43,26 +44,39 @@ class UserView(View):
         else:
             return JsonResponse("Failed to update user information", safe=False)
 
+    # use user = User.objects.get(email=user_data["email"]) to get field value from url -> see the urls.py (<str:email>)
+
+    # query_email = request.GET.get('email', None) - initializing var with value from query parameters
+    # use user = User.objects.get(email=query_email) to get field from query parameters -> /user/?email=...&password=...
+
     def post(self, request, email=""):
-        if email == "":
-            print("Reached email")
+        query_password = request.GET.get('password', None)  # ?password=...
+        query_email = request.GET.get('email', None)  # ?email=...
+
+        if query_email is None:
+            print("Creating user using POST")
             user_data = JSONParser().parse(request)
             user_serializer = UserSerializer(data=user_data)
+
             if user_serializer.is_valid():
                 user_serializer.save()
-                return JsonResponse("New user was created successfully", safe=False)
+                user = User.objects.get(email=user_data["email"])
+                return self.make_response(json.dumps({'email': user.email}))  # user was created. return the email
+                # return JsonResponse("New user was created successfully", safe=False)
             else:
                 return JsonResponse("Failed to create user", safe=False)
-
         else:
-            # search the specified email and return data in json
             try:
-                user = User.objects.get(email=email)
-                user_serializer = UserSerializer(user, many=False)
-                print(user_serializer.data)
-                return self.make_response(json.dumps({'user':user_serializer.data}))
+                print("q_email:", query_email)
+                user = User.objects.get(email=query_email)  # search user with specified email
+                if user.password == query_password:
+                    print("User exists!")
+                    user_serializer = UserSerializer(user, many=False)
+                    return self.make_response(json.dumps({'user': user_serializer.data}))  # user was found. let him log in
+                else:
+                    return self.make_response(json.dumps({'user': ''}))
             except ObjectDoesNotExist:
-            	return self.make_response(json.dumps({'user':''}))
+                return self.make_response(json.dumps({'user': ''}))  # user was not found
 
     def delete(self, request, email=""):
         user = User.objects.get(email=email)
