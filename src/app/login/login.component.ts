6659a30859d8f6ffa028f8bdb291b8f407bb564e
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-12-07 20:34:04 +0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { templateJitUrl } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-login',
