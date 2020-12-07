745876366bd960db8cfab145b3b9115288b76945
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-12-07 20:46:41 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component'
 import {FooterComponent} from './footer/footer.component'
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
