c9f315e772f128cfbe0cef47a9e44eb6ffd9a854
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-12-07 20:18:22 +0200

    Login validation recommit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,42 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { User } from '../user';
 import { templateJitUrl } from '@angular/compiler';
-import { LoginService } from '../login.service'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit {
+  email: string;
+  pwd: string;
+  response: any;
+  constructor(private http: HttpClient, private router: Router) {}
 
-  email:string;
-  pwd:string;
-  response:any;
-  constructor(private http:HttpClient,private router:Router,private loginService:LoginService) { }
-  
-  get loginEmail(){
+  get loginEmail() {
     return this.userEmails.get('loginEmail');
   }
 
+  get loginPassword() {
+    return this.userEmails.get('loginPassword');
+  }
+
   userEmails = new FormGroup({
-    loginEmail: new FormControl('',[
+    loginEmail: new FormControl('', [
       Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    secondaryEmail: new FormControl('',[
-      Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+    ]),
+    loginPassword: new FormControl('', [
+      Validators.required
+    ]),
   });
 
-  enter(){
-    this.loginService.loginEmail(this.email,this.pwd);
+  enter() {
+    this.http
+      .post('http://127.0.0.1:8080/user/' + this.email + '/', {})
+      .subscribe((data) => {
+        if (data['user'] !== '') {
+          this.router.navigate(['/home']);
+        } else {
+          this.pwd = '';
+          alert('User does not exists!');
+        }
+      });
   }
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 }
