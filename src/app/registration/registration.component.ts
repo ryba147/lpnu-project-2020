cf450a7c89d7dee25ce713dbd9c77bdbd9d2ffe8
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-12-15 01:10:11 +0200

    Registration field final validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   AbstractControl,
   FormGroup,
@@ -9,40 +9,44 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { User } from '../interfaces/user';
 import { Router } from '@angular/router';
 import { RegistrationService } from '../registration.service'
-import { from } from 'rxjs';
-function passwordMatcher(c: AbstractControl) {
-  return c.get('password').value === c.get('confirm_password').value
-    ? null
-    : { nomatch: true };
-}
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent {
   user: User = new User();
-  confPass:string;
+  confPass: string;
   form: FormGroup;
-  constructor(public fb: FormBuilder,private http:HttpClient,private router:Router,private registration:RegistrationService) {
+
+  private stringPattern = '^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$';
+
+  constructor(public fb: FormBuilder, private http:HttpClient, private router:Router, private registration:RegistrationService) {
     this.form = this.fb.group({
-      firstname: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$')]],
-      lastname: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$')]],
-      city: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$')]],
-      email: [
-        '',
-        [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')],
-      ],
-      pword: this.fb.group({
-          password: ['', Validators.required], //
-          confirm_password: ['', Validators.required],
-        }, { validator: passwordMatcher }),
-    });
+      firstname: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
+      lastname: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
+      city: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+      confirm_password: ['', [Validators.required, Validators.minLength(8)]]
+    }, {validator: this.passwordMatcher });
   }
+
   get f() { return this.form.controls; }
-  ngOnInit(): void {}
-  register(){
+
+  private passwordMatcher(group: FormGroup): null | { nomatch: boolean } {
+    if (group.get('password') !== null) {
+    const pass = group.get('password').value;
+    const confirm_pass = group.get('confirm_password').value;
+    return pass === confirm_pass
+    // return form.controls['password'].value === c.get('confirm_password').value
+      ? null
+      : { nomatch: true };
+    }
+  }
+
+  register() {
     this.registration.register(this.user);
   }
 }
