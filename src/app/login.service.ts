e8692f33407056b317527d473068dc06ea82b11b
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-12-16 14:18:01 +0200

    Upgrated registration service.

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,28 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { ThisReceiver, ThrowStmt } from '@angular/compiler';
+import { Observable,Subscription } from 'rxjs';
+import { User } from "./interfaces/user.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class LoginService {
-  constructor(private http: HttpClient, private router: Router) { }
-  public loginEmail(email:string, password:string) : void {
-    this.loginMethod(email, password);
-  } 
-
-  private loginMethod(email: string, password: string) {
-    // new HttpParams().set('email', email).set('password', password);
- 
-    this.http.post(`https://eventure-lpnu.herokuapp.com/user/`,{},{params: new HttpParams().set('email', email).set('password', password)}).subscribe(data => {
-      if (data['user'] !== '') {
-        this.router.navigate(['/home']);
-      }
-      else {
-        alert("Wrong user or password");
-      }
-    });
+  private user : User;
+  private status:string;
+  constructor(private http: HttpClient) { }
+  public loginEmail(email:string, password:string): string {
+    this.http.post(`http://127.0.0.1:8080/user/`,{},{params: new HttpParams().set('email', email).set('password', password)})
+      .subscribe(resp => {
+        //console.log(resp.);
+        this.user = resp['user'];
+        this.status = resp['status'];
+      })
+    return this.status;
   }
 }
