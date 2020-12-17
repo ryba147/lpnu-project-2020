71ff5c3a5ee76fec38e2ea65025eac2978ad49d1
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-12-17 15:46:45 +0200

    Updated user service made with observable

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -9,16 +9,9 @@ import { User } from "./interfaces/user.interface";
 })
 
 export class LoginService {
-  private user : User;
-  private status:string;
+  public user : User|null = null;
   constructor(private http: HttpClient) { }
-  public loginEmail(email:string, password:string): string {
-    this.http.post(`http://127.0.0.1:8080/user/`,{},{params: new HttpParams().set('email', email).set('password', password)})
-      .subscribe(resp => {
-        //console.log(resp.);
-        this.user = resp['user'];
-        this.status = resp['status'];
-      })
-    return this.status;
+  public loginEmail(email:string, password:string): Observable<any> {
+    return this.http.post<any>(`http://127.0.0.1:8080/user/`,{},{params: new HttpParams().set('email', email).set('password', password)});
   }
 }
