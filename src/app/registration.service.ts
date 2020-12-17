92e73ed373173bd3331f354c3d191be24d67ab17
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-12-17 21:33:30 +0200

    Working service of registration

--- src/app/registration.service.ts
+++ src/app/registration.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from './interfaces/user.interface';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +10,9 @@ import { User } from './interfaces/user.interface';
 export class RegistrationService {
 
   constructor(private http: HttpClient, private router: Router) { }
-  public register(user:User) : void {
-    this.registerMethod(user);
-  } 
-
-  private registerMethod(user:User):void {
-    console.log(user);
+  public register(user:User) : Observable<any> {
     const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-    this.http.post('https://eventure-lpnu.herokuapp.com/user/',JSON.stringify(user),{headers:headers}).subscribe(data =>{
-      console.log(data);
-      if(data['status']!=='Failed to create user'){
-        this.router.navigate(['/home']);
-      }
-      else{
-        alert('Cannot create user!');
-        user.email = '';
-        user.password = '';
-      }
-    })
-  }                                
+    return this.http.post('http://127.0.0.1:8080/user/',JSON.stringify(user),{headers:headers});
+  }
+
 }
