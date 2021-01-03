98d1cc9891485003a1af24f0ddd09e4a9a1a91da
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-01-03 12:51:10 +0200

    Add files via upload

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,65 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   AbstractControl,
   FormGroup,
   FormBuilder,
   Validators,
 } from '@angular/forms';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user.interface';
 import { Router } from '@angular/router';
-import { RegistrationService } from '../registration.service'
-import {from, Observable} from 'rxjs';
-import {LoginComponent} from "../login/login.component";
-import {LoginService} from "../login.service";
-function passwordMatcher(c: AbstractControl) {
-  return c.get('password').value === c.get('confirm_password').value
-    ? null
-    : { nomatch: true };
-}
+import { RegistrationService } from '../services/registration.service';
+import { LoginService } from '../services/login.service';
+import { Observable } from 'rxjs';
+import { UserProvider } from '../services/user.provider';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent {
   user: User = new User();
-  confPass:string;
+  confPass: string;
   form: FormGroup;
-  response: Observable<any>;
-  responseData: any;
-  responseStatus: string|null = " ";
-  constructor(public fb: FormBuilder, private http:HttpClient, private router:Router, private registration:RegistrationService, private login:LoginService) {
-    this.form = this.fb.group({
-      firstname: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$')]],
-      lastname: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$')]],
-      city: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$')]],
-      email: [
-        '',
-        [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')],
-      ],
-      pword: this.fb.group({
-          password: ['', Validators.required], //
-          confirm_password: ['', Validators.required],
-        }, { validator: passwordMatcher }),
-    });
+  response: number;
+
+  private stringPattern = '^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$';
+
+  constructor(public fb: FormBuilder, private http: HttpClient, private router: Router,
+              private registrationService: RegistrationService, private loginService: LoginService, private userProvider: UserProvider) {
+      this.createForm();
   }
+
   get f() { return this.form.controls; }
-  ngOnInit(): void {}
-  register(){
-    this.response =  this.registration.register(this.user);
-    this.response.subscribe(data =>
-      this.responseData = data
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      firstname: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
+      lastname: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
+      city: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9A-Z._%+-]+@[a-zA-Z.-]+\\.[a-z]{2,4}$')]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]],
+      confirm_password: ['', [Validators.required, Validators.minLength(8)]]
+    }, {validator: this.passwordMatcher });
+  }
+
+  private passwordMatcher(group: FormGroup): null | { nomatch: boolean } {
+    if (group.get('password') !== null) {
+    const pass = group.get('password').value;
+    const confirm_pass = group.get('confirm_password').value;
+    return pass === confirm_pass
+      ? null
+      : { nomatch: true };
+    }
+  }
+
+  register() {
+    this.user.email = this.user.email.toLowerCase();
+    this.registrationService.createUser(this.user).
+    subscribe(data => {
+        this.userProvider.setUser(data['user']);
+        this.response = 201;
+        this.router.navigate(['/home']);
+      },
+      error => {
+          this.response = error.status;
+      }
     );
-    if(this.responseData!==undefined){
-      this.login.user = this.responseData['user'];
-      this.responseStatus = this.responseStatus['status'];}
-    else{
-      this.responseStatus = 'email is not unique';
-    }
-    if(this.responseStatus === 'registered'){
-      this.router.navigate(['/home']);
-    }
   }
 }
