fdb6f9e3689caf77e7d2b618fef17abdd872b603
kkrystyna <khrystynakmit@gmail.com>
2021-01-08 20:54:51 +0200

    oneevent

---
+++ src/app/oneevent/oneevent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oneevent',
+  templateUrl: './oneevent.component.html',
+  styleUrls: ['./oneevent.component.css']
+})
+export class OneeventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
