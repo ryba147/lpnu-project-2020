c494720dd8e31589920635ad0289ce170ad93fd7
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-01-27 19:36:52 +0200

    Add files via upload

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { ThisReceiver, ThrowStmt } from '@angular/compiler';
-import { Observable, Subscription } from 'rxjs';
-import { User } from '../interfaces/user.interface';
-import { UserProvider } from './user.provider'
+import { Observable } from 'rxjs';
+import { UserProvider } from './user.provider';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +10,9 @@ import { UserProvider } from './user.provider'
 export class LoginService {
   constructor(private http: HttpClient, private user: UserProvider) { }
 
-  public loginEmail(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`http://127.0.0.1:8080/users/`, {}, {params: new HttpParams().set('email', email).set('password', password)});
+  public login(email: string, password: string): Observable<object> {
+    return this.http.post<any>(`http://127.0.0.1:8080/users/`, {},
+      {params: new HttpParams().set('email', email).set('password', password)});
   }
 
 }
