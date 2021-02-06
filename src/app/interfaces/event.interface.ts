1d1c6ce4468f5dd296e884068cfceb3acc1cf2db
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-02-06 20:45:10 +0200

    Updated datetime

--- src/app/interfaces/event.interface.ts
+++ src/app/interfaces/event.interface.ts
@@ -2,7 +2,8 @@ export class Event {
   event_name: string;
   event_description: string;
   event_photo: string; // path to the image
-  event_date: Date;
+  event_datetime_begin: Date;
+  event_datetime_end: Date;
   event_rating?: number;
   event_location: string;
 }
