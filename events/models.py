e06f4d56a905fc4a81adfde1387f469c0a8d512b
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-02-08 12:31:14 +0200

    updated-event-fields

--- events/models.py
+++ events/models.py
@@ -4,16 +4,30 @@ from logreg.models import User
 
 # Create your models here.
 class Event(models.Model):
-    event_name = models.CharField(max_length=30, blank=True, null=True)
+    id = models.IntegerField(blank=False, null=False, primary_key=True)
+    event_name = models.CharField(max_length=255, blank=True, null=True)
     event_description = models.TextField(blank=True, null=True)
     event_photo = models.ImageField(blank=True, null=True, upload_to='media/events')
-    # event_date = models.DateTimeField(blank=True, null=True)
     event_datetime_begin = models.DateTimeField(blank=True, null=True)
     event_datetime_end = models.DateTimeField(blank=True, null=True)
     event_rating = models.FloatField(blank=True, null=True)
+    event_location = models.CharField(max_length=255, blank=True, null=True)
+    event_organizer = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.event_name
+
+
+class EventToUser(models.Model):
+    event_id = models.ForeignKey(Event, blank=False, null=True, on_delete=models.CASCADE)
+    user_email = models.ForeignKey(User, blank=False, null=True, on_delete=models.CASCADE)
+
+    STATUS_CHOICES = (
+        ('event_organizer', 'Organizer'),
+        ('to_visit', 'To visit'),
+        ('visited', 'Visited'),
+    )
+    status = models.CharField(max_length=20, blank=True, null=True, choices=STATUS_CHOICES)
 
 
 class Comment(models.Model):
