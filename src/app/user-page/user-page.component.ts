3b3eeb17e9dc8a5b5c8dd3f726fec22ea1d49117
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-02-15 22:33:21 +0200

    Update user-page.component.ts

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,30 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {UserProvider} from '../services/user.provider';
-import {passBoolean} from 'protractor/built/util';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CityProvider} from '../services/ city.provider';
-import {UserpageService} from '../services/userpage.service';
-import {EventsService} from '../services/events.service';
+import { UserProvider } from '../services/user.provider';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CityProvider } from '../services/ city.provider';
+import { UserpageService } from '../services/userpage.service';
+import { EventsService } from '../services/events.service';
 import { User } from '../interfaces/user.interface';
-import { runInThisContext } from 'vm';
-import { flatten, templateJitUrl } from '@angular/compiler';
-import { toUnicode } from 'punycode';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import {defaultGatherDiagnostics} from '@angular/compiler-cli';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.css']
+  styleUrls: ['./user-page.component.css'],
 })
 export class UserPageComponent implements OnInit {
   // 0 - recent, 1 - user pages, 2 - settings
   //Переробити під булеан
   status = 0;
-  loading:boolean;
+  loading: boolean;
   //Перекинути в конструкторі
   currentUser: User;
-  eventList:Array<Event>;
+  eventList: Array<Event>;
   //Забрати
   updatedUser: User;
   autoCompleteChosen: boolean;
@@ -35,8 +29,14 @@ export class UserPageComponent implements OnInit {
   newPass: string;
   confPass: string;
   private stringPattern = '^[a-zA-Zа-яА-ЯіІїЇєЄ-]+$';
-  url:any;
-  constructor(public fb: FormBuilder, private userProvider: UserProvider, private cityProvider: CityProvider, private userPageService: UserpageService, private eventsService: EventsService) {
+  url: any;
+  constructor(
+    public fb: FormBuilder,
+    private userProvider: UserProvider,
+    private cityProvider: CityProvider,
+    private userPageService: UserpageService,
+    private eventsService: EventsService
+  ) {
     this.currentUser = this.userProvider.getUser();
     this.eventList = [];
     this.selectedCities = [];
@@ -46,30 +46,41 @@ export class UserPageComponent implements OnInit {
     this.changePanel(0);
   }
 
-  get f() { return this.form.controls; }
+  get f() {
+    return this.form.controls;
+  }
 
-  readUrl(event:any) {
+  readUrl(event: any) {
     if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
 
       reader.onload = (event: ProgressEvent) => {
         this.url = (<FileReader>event.target).result;
-      }
+      };
 
       reader.readAsDataURL(event.target.files[0]);
     }
   }
 
-  public updateSelectedCities(): void{
+  public updateSelectedCities(): void {
     this.autoCompleteActive = true;
     this.updatedUser.city = this.updatedUser.city.trimLeft();
-    if (this.cityProvider.compareWithSelectedCity(this.updatedUser.city) === true && this.autoCompleteChosen === true) {return; }
+    if (
+      this.cityProvider.compareWithSelectedCity(this.updatedUser.city) ===
+        true &&
+      this.autoCompleteChosen === true
+    ) {
+      return;
+    }
     this.autoCompleteChosen = false;
     this.selectedCities = [];
-    if (this.updatedUser.city.length >= 3){
+    if (this.updatedUser.city.length >= 3) {
       const cities = this.cityProvider.getCityList();
-      for (const city of cities){
-        if (city.substr(0, this.updatedUser.city.length).toLowerCase() === this.updatedUser.city.toLowerCase()){
+      for (const city of cities) {
+        if (
+          city.substr(0, this.updatedUser.city.length).toLowerCase() ===
+          this.updatedUser.city.toLowerCase()
+        ) {
           this.selectedCities.push(city);
         }
       }
@@ -83,8 +94,14 @@ export class UserPageComponent implements OnInit {
     this.newPass = '';
     this.form = this.fb.group({
       profile_photo: [''],
-      firstname: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
-      lastname: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
+      firstname: [
+        '',
+        [Validators.required, Validators.pattern(this.stringPattern)],
+      ],
+      lastname: [
+        '',
+        [Validators.required, Validators.pattern(this.stringPattern)],
+      ],
       city: ['', [Validators.required, Validators.pattern(this.stringPattern)]],
       old_password: [''],
       new_password: [''],
@@ -92,18 +109,27 @@ export class UserPageComponent implements OnInit {
       date: [''],
       pets: [''],
       sex: [''],
-      family_status: ['']
+      family_status: [''],
     });
   }
 
   passwordMatcher(): boolean {
-      if (this.oldPass === '' && this.newPass === '' && this.confPass === '') { return true; }
-      if (this.oldPass === this.currentUser.password && this.newPass === this.confPass && this.confPass.length >= 8 && this.confPass.length <= 12 ) { return true; }
-      return false;
+    if (this.oldPass === '' && this.newPass === '' && this.confPass === '') {
+      return true;
+    }
+    if (
+      this.oldPass === this.currentUser.password &&
+      this.newPass === this.confPass &&
+      this.confPass.length >= 8 &&
+      this.confPass.length <= 12
+    ) {
+      return true;
+    }
+    return false;
   }
 
   setCity(city): void {
-    if (this.cityProvider.setSelectedCity(city) === true){
+    if (this.cityProvider.setSelectedCity(city) === true) {
       this.autoCompleteChosen = true;
       this.updatedUser.city = city;
       this.autoCompleteActive = false;
@@ -112,100 +138,111 @@ export class UserPageComponent implements OnInit {
 
   changePanel(param): void {
     // alert(document.getElementById('accordeonItem1'));
-    for (let i = 0; i <= 2; i++){
-      document.getElementById('accordeonItem' + i.toString()).style.color = '#aaaaaa';
+    for (let i = 0; i <= 2; i++) {
+      document.getElementById('accordeonItem' + i.toString()).style.color =
+        '#aaaaaa';
     }
-    document.getElementById('accordeonItem' + param.toString()).style.color = '#000000';
-    if (param === 0 || param === 1){
+    document.getElementById('accordeonItem' + param.toString()).style.color =
+      '#000000';
+    if (param === 0 || param === 1) {
       this.updateEventList(param);
-    }
-    else{
+    } else {
       this.createForm();
     }
     this.status = param;
   }
 
   updateEventList(mode): void {
-    if (mode === 1){
-      this.eventsService.getEventsList()
-        .subscribe(data => {
-            this.eventList = data.results;
-          },
-          error =>  {
-
-          });
-    }
-    else{
-      this.eventsService.getEventsList()
-        .subscribe(data => {
-            this.eventList = data.results;
-          },
-          error => {
-
-          });
+    if (mode === 1) {
+      this.eventsService.getEventsList().subscribe(
+        (data) => {
+          this.eventList = data['results'];
+        },
+        () => {}
+      );
+    } else {
+      this.eventsService.getEventsList().subscribe(
+        (data) => {
+          this.eventList = data['results'];
+        },
+        () => {}
+      );
     }
   }
 
   updateUser(): void {
     this.loading = true;
-    if (this.newPass.length > 0 && this.passwordMatcher() === true){
+    if (this.newPass.length > 0 && this.passwordMatcher() === true) {
       this.updatedUser.password = this.newPass;
     }
-    this.userPageService.updateUser(this.updatedUser)
-      .subscribe(
-        data => {
-          this.loading = false;
-          this.userProvider.setUser(this.updatedUser);
-          this.currentUser = this.userProvider.getUser();
-        },
-        error => {
-          this.loading = false;
-        }
-      );
+    this.userPageService.updateUser(this.updatedUser).subscribe(
+      () => {
+        this.loading = false;
+        this.userProvider.setUser(this.updatedUser);
+        this.currentUser = this.userProvider.getUser();
+      },
+      () => {
+        this.loading = false;
+      }
+    );
   }
 
   userButtonClick(): void {
     document.getElementById('profile_photo').click();
   }
 
-  resetUser(): void{
+  resetUser(): void {
     this.updatedUser = this.userProvider.getUser();
   }
 
-  calculateDuration(event): string{
+  calculateDuration(event): string {
     const beginEvent = new Date(event.event_datetime_begin);
     const endEvent = new Date(event.event_datetime_end);
-    const diff_in_hours = Math.ceil((endEvent.getTime() - beginEvent.getTime()) / 3600000); // milliseconds in one minute
+    const diff_in_hours = Math.ceil(
+      (endEvent.getTime() - beginEvent.getTime()) / 3600000
+    ); // milliseconds in one minute
     const diff_in_mins = diff_in_hours * 60;
     const diff_in_days = Math.ceil(diff_in_hours / 24);
-    const diff_in_month = Math.ceil( diff_in_hours / 30);
+    const diff_in_month = Math.ceil(diff_in_hours / 30);
     const diff_in_years = Math.ceil(diff_in_hours / 365);
-    if (diff_in_mins < 1) {return '<1 min'; }
-    else
-    if (diff_in_mins < 60) {return diff_in_mins + ' mins'; }
-    else
-    if (diff_in_hours < 24) {return diff_in_hours + ' hrs'; }
-    else
-    if (diff_in_days < 30) { return diff_in_days + ' days'; }
-    else
-    if (diff_in_month > 30) { return diff_in_month + ' month'; }
-    else {
+    if (diff_in_mins < 1) {
+      return '<1 min';
+    } else if (diff_in_mins < 60) {
+      return diff_in_mins + ' mins';
+    } else if (diff_in_hours < 24) {
+      return diff_in_hours + ' hrs';
+    } else if (diff_in_days < 30) {
+      return diff_in_days + ' days';
+    } else if (diff_in_month > 30) {
+      return diff_in_month + ' month';
+    } else {
       return diff_in_years + ' years';
     }
   }
 
-  calculateBeginEnd(event): string{
+  calculateBeginEnd(event): string {
     const beginEvent = new Date(event.event_datetime_begin);
     const endEvent = new Date(event.event_datetime_end);
-    if (beginEvent.getDay() === endEvent.getDay() && beginEvent.getMonth() === endEvent.getMonth() &&  beginEvent.getFullYear() === endEvent.getFullYear()){
-      return beginEvent.toUTCString().substring(0, 22) + ' - ' + endEvent.toTimeString().substring(0, 5);
-    }
-    else{
-      return beginEvent.toUTCString().substring(0, 22) + ' - ' + endEvent.toUTCString().substring(0, 22);
+    if (
+      beginEvent.getDay() === endEvent.getDay() &&
+      beginEvent.getMonth() === endEvent.getMonth() &&
+      beginEvent.getFullYear() === endEvent.getFullYear()
+    ) {
+      return (
+        beginEvent.toUTCString().substring(0, 22) +
+        ' - ' +
+        endEvent.toTimeString().substring(0, 5)
+      );
+    } else {
+      return (
+        beginEvent.toUTCString().substring(0, 22) +
+        ' - ' +
+        endEvent.toUTCString().substring(0, 22)
+      );
     }
   }
 
-  pause(): void{
+  pause(): void {
     alert(1);
   }
 }
