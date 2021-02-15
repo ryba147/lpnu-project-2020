af5339f9f4dc4147f44200fdf1584177a3632815
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-02-15 22:33:14 +0200

    Add files via upload

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -2,11 +2,12 @@ export class User {
   first_name: string;
   last_name: string;
   birth_date: Date;
+  profile_photo?: string;
   email: string;
   password: string;
   city: string;
-  sex?: string = 'not';
-  family_status?: string = 'not specified';
+  sex?: string;
+  family_status?: string;
   pets?: string;
   organizer: boolean;
   rating?: number;
