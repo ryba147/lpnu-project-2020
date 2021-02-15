b61ea7d1c6853445a8ea5cfa8cf95b63fd75324a
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-02-15 16:55:15 +0200

    Updated userpage service

--- src/app/services/userpage.service.ts
+++ src/app/services/userpage.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {User} from '../interfaces/user.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserpageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public updateUser(newUser: User): Observable<object>{
+    const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+    return this.http.put('https://eventure-lpnu.herokuapp.com/users/', JSON.stringify(newUser), {headers : headers });
+  }
 }
 
