bee982e7613bc5950ba8738ce2e4f365b7f54c3d
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-02-16 12:15:26 +0200

    Fixed bug with copying user

--- src/app/services/user.provider.ts
+++ src/app/services/user.provider.ts
@@ -7,12 +7,14 @@ import { User } from "../interfaces/user.interface";
 
 export class UserProvider {
   private user: User;
-  
-  public setUser(resp: User): void { 
-    this.user = resp;
+  public setUser(resp: User): void {
+    this.user = (resp === null)? null: JSON.parse(JSON.stringify(resp));
   }
-  
-  public getUser(): User {
-    return this.user;
+
+  public getUser(): User|null {
+    if(this.user === null)return null;
+    else
+    return JSON.parse(JSON.stringify(this.user));
   }
+
 }
