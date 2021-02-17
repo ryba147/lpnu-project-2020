a52408b12928fd51e04b5916b1f1fb82dfc7f8c9
ryba147 <54861049+ryba147@users.noreply.github.com>
2021-02-18 00:41:50 +0200

    Add files via upload

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,30 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 
 import { Event } from '../interfaces/event.interface';
 import { EventsService } from '../services/events.service';
+import { ResponsePage } from '../interfaces/response-page.interface';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
 })
-
 export class HomePageComponent implements OnInit {
   constructor(private eventsService: EventsService) {}
 
   events: Event[];
-  next: string;
-  previous: string;
+  next_page_num: number | null;
+  previous_page_num: number | null;
+  page_num: number;
+  has_next: boolean;
+  has_prev: boolean;
+
+  page_status: string;
 
   ngOnInit(): void {
     this.reloadData();
   }
 
+  setProperties(resp: ResponsePage): void {
+    this.events = resp.results;
+    this.next_page_num = resp.next_page;
+    this.previous_page_num = resp.previous_page;
+    this.page_num = resp.page;
+    this.has_next = resp.has_next;
+    this.has_prev = resp.has_previous;
+  }
+
   // if page with query number does not exist the {"results": ""} with status 404 will be returned
-  reloadData() {
-    this.eventsService.getEventsList().subscribe(response => {
-      this.events = response['results'];
+  reloadData(): void {
+    this.page_status = "loading";
+    this.eventsService.getPage(1).subscribe(response => {
+      this.setProperties(response);
+      this.page_status = "load_success";
+    }, error => { this.page_status = "load_failed"; }
+    );
+  }
+
+  fetchPrevious(): void {
+    this.eventsService.getPage(this.previous_page_num).subscribe((response) => {
+      this.setProperties(response);
+    });
+  }
+
+  fetchNext(): void {
+    this.eventsService.getPage(this.next_page_num).subscribe((response) => {
+      this.setProperties(response);
     });
   }
 
